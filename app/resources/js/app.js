70a4137ebcbd1b03e30f76ec0ca7c5ac7ae693c7
gomarcd <git@gomarcd.dev>
2024-09-08 10:25:02 -0600

    fix duplicate loading alpine

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -1,9 +1,8 @@
-import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
+import { Livewire } from '../../vendor/livewire/livewire/dist/livewire.esm';
 import Clipboard from '@ryangjchandler/alpine-clipboard'
 import './bootstrap';
 import '../../vendor/masmerise/livewire-toaster/resources/js';
  
 Alpine.plugin(Clipboard)
 window.Alpine = Alpine
-window.Alpine.start()
 Livewire.start()
