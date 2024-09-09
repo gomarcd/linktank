46b7e306690d50c24a3ce92fdf828279ef577274
gomarcd <git@gomarcd.dev>
2024-09-09 07:09:11 -0600

    toaster updates

--- app/resources/views/livewire/search.blade.php
+++ app/resources/views/livewire/search.blade.php
@@ -6,6 +6,18 @@
         </label>
     </div>
 
+    @if ($showUndoButton)
+        @if(session('status'))
+            <div x-data="{ countdown: 5, show: true }" x-init="let timer = setInterval(() => { countdown--; if (countdown <= 0) { show = false; clearInterval(timer); } }, 1000)" x-show="show" class="block items-center p-4 border border-warning rounded-lg mb-4">
+                <button class="text-center w-full py-2 border " wire:click="undoDelete" @click="Toaster.success('Restored!')">
+                    Undo (<span x-text="countdown"></span>)
+                </button>
+            </div>
+        @endif
+    @endif
+
+
+
     @foreach ($this->getBookmarks as $bookmark)
         <div class="flex justify-between items-start p-4 border border-accent rounded-lg mb-4">
             <div class="w-4/5 ml-1">
@@ -32,15 +44,19 @@
                 </button>
 
                 <!-- Delete Button -->
-                <button type="button" wire:click="delete({{ $bookmark->id }})" class="hover:text-red-600">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                    </svg>
+                <button 
+                    type="button" 
+                    wire:click="delete({{ $bookmark->id }})" 
+                    class="hover:text-red-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                        </svg>
                 </button>
+                
             </div>
         </div>
     @endforeach
-
-    <!-- Infinite scroll trigger -->
+    
+        <!-- Infinite scroll trigger -->
     <div class="h-16" x-intersect.full="$wire.loadMore()"></div>
 </div>
