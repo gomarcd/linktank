014e79050764432bc46170c3736604b26f819308
gomarcd <git@gomarcd.dev>
2024-09-18 17:04:41 -0600

    update route to /

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Search;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/search', Search::class);
+Route::get('/', Search::class);
